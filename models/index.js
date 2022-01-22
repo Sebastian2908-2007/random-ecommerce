35bb850bb8bcf525f319d927a032e8ac0143f53e
Sebabin <sebastiancrespin279@gmail.com>
2022-01-21 17:47:51 -0700

    have all models made as well as proper data associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+   foreignKey: 'category_id'
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Each productTag is made with a product_id
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
-// Tags belongToMany Products (through ProductTag)
+// productTag always is made with a tag_id
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
